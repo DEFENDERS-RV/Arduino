5eec8724fc32f4be49a215145ebe8846a158578a
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-01-23 23:23:55 -0600

    Creacion del repo para Arduino

---
+++ SE_1_U1_EQ_3/Arduino/Prog_1_Intro/Prog_1_Intro.ino
@@ -0,0 +1,27 @@
+
+//Declaracion /asignacion de variables... ES MUY SIMILAR A JAVA
+
+byte b;
+
+short s;
+int i; // ocupa 2 bits en Arduino
+long l;
+char c;
+bool boo;
+double d;
+float f;
+String cadena;
+
+//Considerar que la capacidad del tipo de dato difiere en algunos casos del de JAVA.
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
