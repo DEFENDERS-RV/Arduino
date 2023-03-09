328ae106e53c4bbe3f5ecf33ae04f5ad37ef9c4c
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-03-08 23:00:48 -0600

    Boxplot Basico Ej1

---
+++ SE_I_U3_EQ_3/Python/BoxplotEJ1/main.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+datos = [108 , 31 , 75 , 87 , 79 , 88 , 89 , 118 , 51 , 89 , 174 , 95 , 51 , 70 , 73]
+
+plt.boxplot(datos)
+plt.title("Boxplot Basico")
+plt.show()
