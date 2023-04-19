17eb1eebf23eb82f193c5890f432ddcc4915d00c
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-04-18 23:16:51 -0600

    Instalacion del NODEJS

--- SE_1_U2_EQ_3/Arduino/ControlGBA_Arduino/ControlGBA_Arduino.ino
+++ SE_1_U2_EQ_3/Arduino/ControlGBA_Arduino/ControlGBA_Arduino.ino
@@ -2,24 +2,24 @@ int upPin = 2;
 int downPin = 3;
 int leftPin = 4;
 int rightPin = 5;
-int xPin = 6;
-int zPin = 7;
+int aPin = 6;
+int xPin = 7;
 int selectPin = 8;
 int startPin = 9;
-int aPin = 10;
-int sPin = 11;
+int sPin = 10;
+int zPin = 11;
 
 void setup() {
   pinMode(upPin, INPUT_PULLUP);
   pinMode(downPin, INPUT_PULLUP);
   pinMode(leftPin, INPUT_PULLUP);
   pinMode(rightPin, INPUT_PULLUP);
+  pinMode(aPin, INPUT_PULLUP);
   pinMode(xPin, INPUT_PULLUP);
-  pinMode(zPin, INPUT_PULLUP);
   pinMode(selectPin, INPUT_PULLUP);
   pinMode(startPin, INPUT_PULLUP);
-  pinMode(aPin, INPUT_PULLUP);
   pinMode(sPin, INPUT_PULLUP);
+  pinMode(zPin, INPUT_PULLUP);
   Serial.setTimeout(100);
   Serial.begin(9600);
 }
@@ -28,12 +28,12 @@ int up = 0;
 int down = 0;
 int left = 0;
 int right = 0;
-int x = 0;
-int z = 0;
 int a = 0;
-int s = 0;
+int x = 0;
 int select = 0;
 int start = 0;
+int s = 0;
+int z = 0;
 
 void loop() {
   if (digitalRead(upPin) == LOW) {
@@ -52,13 +52,13 @@ void loop() {
     right = (right == 1) ? 0 : 1;
     Serial.println(right);
   }
-  if (digitalRead(xPin) == LOW) {
-    x = (z == 1) ? 0 : 1;
+  if (digitalRead(aPin) == LOW) {
+    a = (a == 1) ? 0 : 1;
     Serial.println(a);
   }
-  if (digitalRead(zPin) == LOW) {
-    z = (z == 1) ? 0 : 1;
-    Serial.println(z);
+  if (digitalRead(xPin) == LOW) {
+    x = (x == 1) ? 0 : 1;
+    Serial.println(x);
   }
   if (digitalRead(selectPin) == LOW) {
     select = (select == 1) ? 0 : 1;
@@ -68,13 +68,13 @@ void loop() {
     start = (start == 1) ? 0 : 1;
     Serial.println(start);
   }
-    if (digitalRead(aPin) == LOW) {
-    a = (a == 1) ? 0 : 1;
-    Serial.println(a);
-  }
-    if (digitalRead(sPin) == LOW) {
+  if (digitalRead(sPin) == LOW) {
     s = (s == 1) ? 0 : 1;
     Serial.println(s);
   }
-  delay(10);
+  if (digitalRead(zPin) == LOW) {
+    z = (z == 1) ? 0 : 1;
+    Serial.println(z);
+  }
+  delay(50);
 }
