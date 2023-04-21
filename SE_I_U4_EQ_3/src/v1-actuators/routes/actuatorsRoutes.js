91a1d23c57fe4fadbef86dd0f1003107333f6d22
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-04-20 23:20:55 -0600

    Preparando el CRUD
    -Configurando el index.js para que posteiormente en postman nos arroje lo siguiente: res.status(210).send({dato1:'valor1' , dato2: 'valor2'})

--- SE_I_U4_EQ_3/src/v1-actuators/routes/actuatorsRoutes.js
+++ SE_I_U4_EQ_3/src/v1-actuators/routes/actuatorsRoutes.js
@@ -4,12 +4,28 @@ const express = require('express')
 const router = express.Router()
 
 router
-.get("/" , function(req,res){})
-.get("/:idActuator" , function(req,res){})
-.get("/idActuator/registros" , function(req,res){})
-.post("/" , function(req,res){})
-.patch("/:idActuator",function(req,res){})
-.delete("/:idActuator" , function(req,res){});
+.get("/" ,
+ function(req,res){
+    console.log('SELECT * FROM ...')
+    res.status(210).send({dato1:'valor1' , dato2: 'valor2'})
+
+ }
+ )//EQUIVALENTE EN SQL-CRUD: SELECT * FROM...
+.get("/:idActuator" , function(req,res){
+    console.log('SELECT*FROM...WHERE')
+}) //EQUIVALENTE EN SQL - CRUD: SELECT*FROM..WHERE
+.get("/idActuator/registros" , function(req,res){
+    console.log('SELECT * FROM ... INNER JOIN...WHERE')
+})
+.post("/" , function(req,res){
+    console.log('INSERT')
+}) //EQUIVALENTE EN SQL-CRUD:INSERT
+.patch("/:idActuator",function(req,res){
+    console.log('UPDATE')
+})//EQUIVALENTE EN SQL-CRUD: INSERT
+.delete("/:idActuator" , function(req,res){
+    console.log('DELETE * FROM ...')
+});
 
 
 module.exports = router;
